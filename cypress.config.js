c5ac69cd2a3e4e63c32f13b9d1e49d9c063f6810
Aaron_Zhou <clark1729@outlook.com>
2023-10-20 21:37:16 +0800

    chore: add `"type": "module"`

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,5 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
-    e2e: {
-        // We've imported your old cypress plugins here.
-        // You may want to clean this up later by importing these.
-        setupNodeEvents(on, config) {
-            return require('./cypress/plugins/index.js')(on, config)
-        }
-    }
+export default defineConfig({
+    e2e: {}
 })
